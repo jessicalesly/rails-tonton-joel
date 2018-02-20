69430a93b88228a57760fa11a90f58c027e70676
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-02-20 18:51:20 +0100

    index done with design

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["La petite carresse antillaise", "La douceur des îles "],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
