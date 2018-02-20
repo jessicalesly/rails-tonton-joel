40187cff17cf52ecf972ad7f84e163ab5bd7f913
Clement Simons <clementsimons@gmail.com>
2018-02-20 12:51:36 +0100

    finalized orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
   resources :rums, only: [:index, :show, :new, :create, :edit, :update] do
     resources :orders, only: [:create]
   end
   resources :orders, only: [:index, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
