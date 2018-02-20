46f4f0878f248bb57cbb9e4a8697222528efc794
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-02-20 12:51:39 +0100

    Rums index and show created (no design)

---
+++ app/controllers/rums_controller.rb
@@ -0,0 +1,32 @@
+class RumsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @rums = Rum.all
+  end
+
+  # def new
+  #   @rum = Rum.new()
+  # end
+
+
+  # def create
+  #   @rum = Rum.new(rum_params)
+  #   if @rum.save
+  #     redirect_to rum_path(@rum)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  def show
+    @rum = Rum.find(params[:id])
+  end
+
+  private
+
+  def rum_params
+    params.require(:rum).permit(:name, :description, :quantity, :price)
+  end
+
+end
