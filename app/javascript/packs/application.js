7d11c4feea82150e21ee1bc727c80569a7794b25
Loic Chesneau <loic.chesneau@lrqdo.fr>
2018-02-21 15:48:38 +0100

    js back on the homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
 import "bootstrap";
 import { switch_tabs } from './switch_tabs';
-import { loadDynamicBannerText } from '../components/sweetalert';
+import { loadDynamicBannerText } from '../components/banner';
 import { bindSweetAlertButtonDemo } from '../components/sweetalert';
 
 if (document.getElementById("tab_left")) {
   switch_tabs();
 }
 
-if (document.getElementById("#banner-typed-text)")) {
+if (document.getElementById("banner-typed-text")) {
   loadDynamicBannerText();
 }
 
