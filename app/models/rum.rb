643621800f2bb16c1f0d4d8ffeb5453c19eab3a0
Loic Chesneau <loic.chesneau@lrqdo.fr>
2018-02-22 11:05:01 +0100

    gem installed and set up in the Rum model

--- app/models/rum.rb
+++ app/models/rum.rb
@@ -4,4 +4,11 @@ class Rum < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :volume, presence: true
+
+  include PgSearch
+  pg_search_scope :search_rum,
+    against: [ :name, :description ],
+    using: {
+        tsearch: { prefix: true }
+      }
 end
