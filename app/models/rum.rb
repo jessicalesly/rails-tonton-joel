d8e7da0ac04ebda4526ac4f0202fe52fd0db3c9e
Clement Simons <clementsimons@gmail.com>
2018-02-23 11:23:28 +0100

    improving the seed

--- app/models/rum.rb
+++ app/models/rum.rb
@@ -1,8 +1,8 @@
 class Rum < ApplicationRecord
   belongs_to :user
   has_many :orders
-  mount_uploader :photo, PhotoUploader
   has_many :reviews
+  mount_uploader :photo, PhotoUploader
   validates :name, presence: true
   validates :price, presence: true
   validates :volume, presence: true
