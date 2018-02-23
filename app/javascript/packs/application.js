f58b2cfb45202797da23abf79caad49d6510ded5
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-02-23 15:31:17 +0100

    adding design show page + effect scroll on show and index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "bootstrap";
 import { switch_tabs } from './switch_tabs';
 import { loadDynamicBannerText } from '../components/banner';
 import { bindSweetAlertButtonDemo } from '../components/sweetalert';
+import { revealOnScroll} from "../components/reveal_on_scroll";
+revealOnScroll();
 
 if (document.getElementById("tab_left")) {
   switch_tabs();
